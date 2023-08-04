ed6e8b0d0b0559cab5e76646efcece97f51f3799
ohjungmin <jmin980317@naver.com>
2023-08-04 16:18:50 +0900

    add ddq

--- rl/agents/__init__.py
+++ rl/agents/__init__.py
@@ -4,7 +4,7 @@ from rl.agents.reinforce.reinforce import REINFORCE
 from rl.agents.a2c.a2c import A2C
 # from rl.agents.ppo.ppo import PPO
 from rl.agents.dqn.dqn import DQN
-# from rl.agents.ddqn.ddqn import DDQN
+from rl.agents.ddqn.ddqn import DDQN
 
 REGISTRY = {}
 
@@ -12,4 +12,4 @@ REGISTRY["reinforce"] = REINFORCE
 REGISTRY["a2c"] = A2C
 # REGISTRY["ppo"] = PPO
 REGISTRY["dqn"] = DQN
-# REGISTRY["ddqn"] = DDQN
+REGISTRY["ddqn"] = DDQN
